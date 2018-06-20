b5783f84cb88fdec27bc44db38fbc91bcbfe83c1
xfy1233 <x983796531@163.com>
2018-06-20 18:09:36 +0800

    1

---
+++ src/main/java/cn/e3mall/portal/controller/IndexController.java
@@ -0,0 +1,26 @@
+package cn.e3mall.portal.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * 首页展示Controller
+ * <p>
+ * Title: IndexController
+ * </p>
+ * <p>
+ * Description:
+ * </p>
+ * <p>
+ * Company: www.itcast.cn
+ * </p>
+ * 
+ * @version 1.0
+ */
+@Controller
+public class IndexController {
+	@RequestMapping("/index")
+	public String showIndex() {
+		return "index";
+	}
+}
